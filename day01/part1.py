a071a1a03f62ca7333c7dbf8768d62e74fc669a2
Peter Gürtler <coder@sonensei.ch>
2022-12-01 06:11:28 +0100

    Day 01

--- day01/part1.py
+++ day01/part1.py
@@ -6,9 +6,28 @@ else:
     p = sys.argv[1]
 
 
-with open("input1.txt") as f:
+with open("input2.txt") as f:
     lines = f.read().splitlines()
 
-
+    cals = []
+    curr = []
     for l in lines:
-        print(p, l)
+        if len(l.strip()) == 0:
+            cals.append(curr)
+            curr = []
+        else:
+            curr.append(int(l))
+    if len(curr) > 0:
+        cals.append(curr)
+
+    sums = list(sum(c) for c in cals)
+    ss = sorted(sums)
+    #print(ss)
+    print(sum(ss[-3:]))
+    exit(0)
+    max_cal = 0
+    for c in cals:
+        s = sum(c)
+        if s > max_cal:
+            max_cal = s
+    print(max_cal)
