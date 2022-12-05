010c363918e8a1bcbff826e082d33638de228569
Peter Gürtler <coder@sonensei.ch>
2022-12-05 08:07:51 +0100

    Day 5

--- day04/part2.py
+++ day04/part2.py
@@ -8,12 +8,11 @@ else:
 def parse_range(r):
     return list(int(i) for i in r.split('-'))
 
-def full_overlap(r1, r2):
-    if r1[0] >= r2[0] and r1[1] <= r2[1]:
-        return True
-    if r2[0] >= r1[0] and r2[1] <= r1[1]:
-        return True
-    return False
+def inrange(val, rng):
+    return val >= rng[0] and val <= rng[1]
+
+def overlap(r1, r2):
+    return inrange(r1[0], r2) or inrange(r1[1], r2) or inrange(r2[0], r1) or inrange(r2[1], r1)
 
 with open(filename) as f:
     lines = f.read().splitlines()
@@ -23,6 +22,6 @@ with open(filename) as f:
         parts = l.split(',')
         r1 = parse_range(parts[0])
         r2 = parse_range(parts[1])
-        if full_overlap(r1, r2):
+        if overlap(r1, r2):
             count += 1
     print(count)
---
+++ day05/part1.py
@@ -0,0 +1,47 @@
+import sys
+
+if len(sys.argv) <= 1:
+    raise Exception("No input file specified")
+else:
+    filename = sys.argv[1]
+
+def extract_move(line):
+    parts = line.split(" ")
+    return (int(parts[1]), int(parts[3]) - 1, int(parts[5]) - 1)
+
+with open(filename) as f:
+    lines = f.read().splitlines()
+    
+    grid_lines = []
+    move_lines = []
+    mode_grid = True
+    for l in lines:
+        if mode_grid:
+            if len(l.strip()) == 0:
+                mode_grid = False
+            else:
+                grid_lines.append(l)
+        else:
+            move_lines.append(l)
+    grid_lines.reverse()
+    chunks = grid_lines[1].split(" ")
+    stacks = list([] for l in chunks)
+    print(stacks)
+    for row in grid_lines[1:]:
+        ix = 0
+        col = 0
+        while ix <= len(row):
+            bl = row[ix:ix+3]
+            print(str(ix) + "  *" + bl + "*")
+            if len(bl.strip()) > 0:
+                stacks[col].append(bl[1])
+            ix += 4
+            col += 1
+    print(stacks)
+    for m in move_lines:
+        count, col_from, col_to = extract_move(m)
+        print("Move", count, col_from, col_to)
+        for i in range(count):
+            stacks[col_to].append(stacks[col_from].pop())
+        print(stacks)
+    print("".join(c.pop() for c in stacks))
