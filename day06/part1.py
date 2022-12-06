86d8294dad0f3cd0c6ddadd40ee34bb4d63b4fcf
Peter Gürtler <coder@sonensei.ch>
2022-12-06 07:00:53 +0100

    Cleanup day 6

--- day06/part1.py
+++ day06/part1.py
@@ -5,15 +5,11 @@ if len(sys.argv) <= 1:
 else:
     filename = sys.argv[1]
 
-
 with open(filename) as f:
     inp = f.read()
-
     s = 0
     while True:
-        chars = set()
-        chars.update(inp[s:s+14])
-        if len(chars) >= 14:
+        if len(set(inp[s:s+14])) >= 14:
             print(s + 14)
             exit(0)
         s += 1
