a2c9902477097d238ee21636b4b61c934f139ff0
Peter Gürtler <coder@sonensei.ch>
2022-12-08 06:29:06 +0100

    Day 8

--- day08/part2.py
+++ day08/part2.py
@@ -5,20 +5,22 @@ if len(sys.argv) <= 1:
 else:
     filename = sys.argv[1]
 
-def is_visible_dir(forrest, x, y, direction):
+def score_dir(forrest, x, y, direction):
+    score = 0
     h = forrest[x][y]
     while x > 0 and x < len(forrest) - 1 and y > 0 and y < len(forrest[x]) - 1:
         x += direction[0]
         y += direction[1]
+        score += 1
         if forrest[x][y] >= h:
-            return False
-    return True
+            break
+    return score
 
-def is_visible(forrest, x, y):
+def score(forrest, x, y):
+    total_score = 1
     for direction in [ (0, 1), (0, -1), (1, 0), (-1, 0) ]:
-        if is_visible_dir(forrest, x, y, direction):
-            return True
-    return False
+        total_score *= score_dir(forrest, x, y, direction)
+    return total_score
 
 with open(filename) as f:
     lines = f.read().splitlines()
@@ -26,10 +28,11 @@ with open(filename) as f:
     forrest = []
     for l in lines:
         forrest.append(list([int(h) for h in l]))
-    visible_count = 0
+    max_score = 0
     for y in range(len(forrest)):
         row = forrest[y]
         for x in range(len(row)):
-            if is_visible(forrest, x, y):
-                visible_count += 1
-    print(visible_count)
+            s = score(forrest, x, y)
+            if s > max_score:
+                max_score = s
+    print(max_score)
