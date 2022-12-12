5277d422b119465fa59ee7c3c812b749d595fb9f
Peter Gürtler <coder@sonensei.ch>
2022-12-12 07:34:10 +0700

    Day 11

--- day11/part2.py
+++ day11/part2.py
@@ -25,13 +25,7 @@ class Op:
     def eval(self, val):
         o1 = val if self.arg1 == 'old' else self.arg1
         o2 = val if self.arg2 == 'old' else self.arg2
-        ##print("evaluation: ")
-        ##print(self.op)
-        ##print(self.arg1)
-        ##print(self.arg2)
-        ##print(val)
         r = self.op(o1, o2)
-        ##print(r)
         return r
 
 class DivTest:
@@ -56,8 +50,8 @@ def build_operation(formula):
         raise Exception(parts[1])
     arg0 = (int(parts[0]) if parts[0].isnumeric() else parts[0])
     arg1 = (int(parts[2]) if parts[2].isnumeric() else parts[2])
-    ##print("Build Op: " + str(formula) + ": " + str(op) + " / " + str(arg0) + " / " + str(arg1))
     return Op(op, arg0, arg1)
+
 def create_monkey(lines):
     index = int(lines[0].split(' ')[1].split(':')[0])
     starting_items = list( [int(i) for i in lines[1].split(": ")[1].split(",")])
@@ -71,21 +65,43 @@ def create_monkey(lines):
 
 def process_monkey_turn(index, monkeys):
     curr_m = monkeys[index]
-    print("Monkey " + str(curr_m.index))
+    #print("Monkey " + str(curr_m.index))
     for i in curr_m.items:
         curr_m.inspected_count += 1
-        print("  inspect " + str(i))
+        #print("  inspect " + str(i))
         w = curr_m.operation.eval(i)
-        print("    worry level " + str(w))
-        b = w // 3
-        print("    bored " + str(b))
+        #print("    worry level " + str(w))
+        b = w #// 3
+        #print("    bored " + str(b))
         t = curr_m.test.test(b)
-        print("    Test is " + str(t))
+        #print("    Test is " + str(t))
         next_m_id = curr_m.tgt_monkey_true if t else curr_m.tgt_monkey_false
-        print("    Throw " + str(b) + " to monkey " + str(next_m_id))
+        #print("    Throw " + str(b) + " to monkey " + str(next_m_id))
         monkeys[next_m_id].items.append(b)
     curr_m.items = []
 
+def make_smaller(p, monkeys):
+    smallest = None
+    print(p)
+    #for m in monkeys.values():
+    #    for i in m.items:
+    #        if i > 0 and i < p:
+    #            #print("no " + str(i))
+    #            return False
+    #        if smallest is None or i < smallest:
+    #            smallest = i
+    #print("make smaller (smallest is " + str(smallest) + "}")
+    #if smallest == 0:
+    #    for m in monkeys.values():
+    #        print(m.items)
+    #d = smallest // p
+    #print("smallest " + str(smallest) + " / " + str(d))
+    for m in monkeys.values():
+        #print(m.items)
+        m.items = list([ i % p for i in m.items])
+        #print(m.items)
+    #print("yes")
+    return True
 
 with open(sys.argv[1]) as f:
     lines = f.read().splitlines()
@@ -99,16 +115,19 @@ with open(sys.argv[1]) as f:
         grouper.add(l)
 
     monkeys = {}
+    prod = 1
     for g in grouper.groups:
         m = create_monkey(g)
+        prod *= m.test.v
         monkeys[m.index] = m
-
-    for turn in range(20):
+    #print(prod)
+    #for turn in range(20):
+    for turn in range(10000):
         for index in sorted(monkeys.keys()):
-            print()
+            #print()
             process_monkey_turn(index, monkeys)
         print("After turn " + str(turn + 1))
-
+        make_smaller(prod, monkeys)
         for index in sorted(monkeys.keys()):
             print("  Monkey  " + str(index) + ": " + str(monkeys[index].items))
     ic = sorted([m.inspected_count for m in monkeys.values()])
